d267e91e122040828ca78eb4ae98c7dffd5caa2f
mohammad reza hassani <mrezahassani@protonmail.com>
2023-07-16 04:27:20 +0330

    fix a bug

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -6,21 +6,22 @@ use error::Error;
 use expression::Expression;
 use literal::Literal;
 
-pub struct Ema;
-
-impl Ema {
-    pub fn eval(exp: Expression) -> Result<Literal, Error> {
-        match exp {
+pub fn eval(exp: Expression) -> Result<Literal, Error> {
+    match exp {
+        Expression::Literal(l) => match l {
+            Literal::Number(num) => Ok(Literal::Number(num)),
+            Literal::String(str) => Ok(Literal::String(str[1..str.len() - 1].to_string())),
+            _ => Err(Error::Reason("unimplemented".to_string())),
+        },
+        Expression::List(exp) => match &exp[0] {
             Expression::Literal(l) => match l {
-                Literal::Number(num) => Ok(Literal::Number(num)),
-                Literal::String(str) => Ok(Literal::String(str)),
+                Literal::String(str) => match str.as_str() {
+                    "+" => Err(Error::Reason("unimplemented".to_string())),
+                    _ => Err(Error::Reason("unimplemented".to_string())),
+                },
                 _ => Err(Error::Reason("unimplemented".to_string())),
             },
-            Expression::List(exp) => {
-                //
-
-                return Err(Error::Reason("unimplemented".to_string()));
-            }
-        }
+            _ => Err(Error::Reason("unimplemented".to_string())),
+        },
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,14 @@
-use ema::{expression::Expression, literal::Literal, Ema};
+use ema::{eval, expression::Expression, literal::Literal};
 
 #[test]
 fn self_evaluate_exp() {
     assert_eq!(
-        Ema::eval(Expression::Literal(Literal::Number(1.0))),
+        eval(Expression::Literal(Literal::Number(1.0))),
         Ok(Literal::Number(1.0))
     );
 
-    let string = "'hello'".to_string();
     assert_eq!(
-        Ema::eval(Expression::Literal(Literal::String(string.clone()))),
-        Ok(Literal::String(string.clone()))
+        eval(Expression::Literal(Literal::String("'hello'".to_string()))),
+        Ok(Literal::String("hello".to_string())),
     );
 }
