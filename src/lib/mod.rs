0f5508c7d05130991b4cd22404f2c27995cfb285
mohammad reza hassani <mrezahassani@protonmail.com>
2023-07-16 07:31:23 +0330

    sum operation

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -4,23 +4,34 @@ pub mod expression;
 use error::Error;
 use expression::Expression;
 
-pub fn eval_exp(exp: Expression) -> Result<Expression, Error> {
+pub fn eval_exp(exp: &Expression) -> Result<Expression, Error> {
     match exp {
-        Expression::Number(num) => Ok(Expression::Number(num)),
+        Expression::Number(num) => Ok(Expression::Number(*num)),
         Expression::String(str) => Ok(Expression::String(str[1..str.len() - 1].to_string())),
-        Expression::List(exp) => {
-            let head = &exp[0];
-            match head {
-                Expression::String(str) => match str.as_str() {
-                    "+" => {
-                        todo!()
-                    }
-                    _ => todo!(),
-                },
-                _ => Err(Error::Reason("unimplemented".to_string())),
-            }
-        }
+        Expression::List(list) => eval_list(list.to_vec()),
 
+        _ => Err(Error::Reason("unimplemented".to_string())),
+    }
+}
+
+fn eval_list(list: Vec<Expression>) -> Result<Expression, Error> {
+    use Expression::*;
+
+    let head = &list[0];
+    let left = eval_exp(&list[1])?;
+    let right = eval_exp(&list[2])?;
+
+    match head {
+        Expression::String(str) => match str.as_str() {
+            "+" => match (left, right) {
+                (Number(l), Number(r)) => Ok(Number(r + l)),
+                (String(l), String(r)) => Ok(String(l + &r)),
+                _ => Err(Error::Reason(
+                    "+ op unimplemented for given value types".to_string(),
+                )),
+            },
+            _ => todo!(),
+        },
         _ => Err(Error::Reason("unimplemented".to_string())),
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,21 +1,66 @@
 use ema::{eval_exp, expression::Expression};
 
+use Expression::*;
 #[test]
 fn self_evaluate_exp() {
-    use Expression::*;
-    assert_eq!(eval_exp(Number(1.0)), Ok(Number(1.0)));
+    assert_eq!(eval_exp(&Number(1.0)), Ok(Number(1.0)));
 
     assert_eq!(
-        eval_exp(String("'hello'".to_string())),
+        eval_exp(&String("'hello'".to_string())),
         Ok(String("hello".to_string())),
     );
 
     assert_eq!(
-        eval_exp(List(vec![
+        eval_exp(&List(vec![
             String("+".to_string()),
             Number(2.0),
             Number(5.0),
         ])),
         Ok(Number(7.0)),
+    );
+}
+
+#[test]
+fn math_op() {
+    // (+ 2 5)
+    assert_eq!(
+        eval_exp(&List(vec![
+            String("+".to_string()),
+            Number(2.0),
+            Number(5.0),
+        ])),
+        Ok(Number(7.0)),
+    );
+
+    // (+ (+ 5 5) 5) = 15
+    assert_eq!(
+        eval_exp(&List(vec![
+            String("+".to_string()),
+            List(vec![String("+".to_string()), Number(5.0), Number(5.0),]),
+            Number(5.0),
+        ])),
+        Ok(Number(15.0)),
+    );
+
+    // (+ (+ 5 (+ 5 5)) 5) = 15
+    // assert_eq!(
+    //     eval_exp(&List(vec![
+    //         String("+".to_string()),
+    //         List(vec![String("+".to_string()), Number(5.0), Number(5.0),]),
+    //         Number(5.0),
+    //     ])),
+    //     Ok(Number(20.0)),
+    // );
+}
+
+#[test]
+fn sum_op_for_string() {
+    assert_eq!(
+        eval_exp(&List(vec![
+            String("+".to_string()),
+            String("'Hello'".to_string()),
+            String("'World'".to_string()),
+        ])),
+        Ok(String("HelloWorld".to_string())),
     );
 }
