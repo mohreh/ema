59362224878032d4cf66104f00a2f11333e1bb93
mohammad reza hassani <mrezahassani@protonmail.com>
2023-08-01 15:20:29 +0330

    implement dispaly for error enum

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Error {
     Invalid(String),
@@ -10,5 +12,19 @@ pub enum Error {
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
         Error::Reason(value.to_string())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Error::*;
+        let str = match self {
+            Invalid(err) => format!("invalid statement: {}", err),
+            Parse(err) => format!("parsing error: {}", err),
+            Reason(err) => err.to_string(),
+            Reference(err) => format!("reference error: {}", err),
+            Token(err) => format!("missing token: {}", err),
+        };
+        write!(f, "{}", str)
     }
 }
